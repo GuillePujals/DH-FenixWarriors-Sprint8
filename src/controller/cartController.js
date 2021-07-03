bb1ddbd003e996a205b5c9500aa45255e0cc7240
Andres Ottonello <andres.ottonello@edessa.com.ar>
2021-07-03 17:48:10 -0300

    Cart v3

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -11,13 +11,30 @@ let cartController = {
             .then(property => {
                 let user = req.session.userLogged;
                 if (property) {
-                    console.log(property);
-                    console.log("-------------------------")
+                    // console.log(property);
+                    // console.log("-------------------------")
                     res.render('products/productCart', {property, user});
                 } else {
                     res.render('error404');
                 }
             });
+    },
+    totalCost: (idPoperty, days) => {
+        db.Property.findByPk(idPoperty)
+        .then(property => {
+            // console.log(property.price + " dias "+ days);
+            return (property.price * days);
+        });
+    },
+    payProcess: (req, res) => {
+        console.log("-------------------------")
+        console.log(req.params.id)
+        console.log(req.body);
+
+        let totalCost = cartController.totalCost(req.params.id, 2);
+        console.log("Total" + totalCost);
+
+        // db.Order.create        
     }
 }
 
