a1c6eb418833ad62bd886a618e6d7d5b8938fb55
Andres Ottonello <andres.ottonello@edessa.com.ar>
2021-07-16 14:29:10 -0300

    Mejoras validación front Create Prod.

--- public/js/createProduct.js
+++ public/js/createProduct.js
@@ -1,11 +1,76 @@
 window.onload = function(){
-
     let form = document.querySelector('.form_edit');
+    let address = document.querySelector('#address');
+    let foto = document.querySelector('#foto');
+    let price = document.querySelector('#price');
+    let n_people = document.querySelector('#n_people');
+
+
+
+    let acceptedExtensions = ['JPG', 'jpg', 'png', 'gif', 'jpeg'];
+    let errors = [];
+
+    address.addEventListener('blur', () => {    
+        let feedback = ''; 
+        let feedbackElement = address.nextElementSibling;
+       
+        // console.log(feedbackElement);
+        if(address.value.trim() == ""){
+            feedback = 'La dirección no puede quedar vacía'
+        } else if(address.value.length < 2){
+            feedback = 'La dirección debe tener 2 o mas caracteres'
+        }
+        // console.log(feedbackElement);
+
+        if (feedback) {
+            feedbackElement.innerText = feedback
+            errors.address = feedback;
+        } else {
+            feedbackElement.innerText = '';
+        }
+    });
+    price.addEventListener('blur', () => {    
+        let feedback = ''; 
+        let feedbackElement = price.nextElementSibling;
+       
+        // console.log(feedbackElement);
+        if(price.value.trim() == ""){
+            feedback = 'Debe completar el precio por noche'
+        } else if(Number(price.value) <= 0 ){
+            feedback = 'El precio debe ser mayor que cero'
+        }
+        // console.log(feedbackElement);
+
+        if (feedback) {
+            feedbackElement.innerText = feedback
+            errors.price = feedback;
+        } else {
+            feedbackElement.innerText = '';
+            // console.log(feedback);
+        }
+    });
+
+    n_people.addEventListener('blur', () => {    
+        let feedback = ''; 
+        let feedbackElement = n_people.nextElementSibling;
+       
+        if(n_people.value.trim() == ""){
+            feedback = 'Debe completar la capacidad'
+        } else if(Number(n_people.value) <= 0 ){
+            feedback = 'La capacidad debe ser mayor que cero'
+        }
+
+        if (feedback) {
+            feedbackElement.innerText = feedback
+            errors.n_people = feedback;
+        } else {
+            feedbackElement.innerText = '';
+        }
+    });
+
+
     form.addEventListener('submit', (e) => {
 
-        let errors = [];
-        let address = document.querySelector('#address');
-        let foto = document.querySelector('#foto');
 
         if (address.value == ''){
             errors.push('Complete la dirección');
