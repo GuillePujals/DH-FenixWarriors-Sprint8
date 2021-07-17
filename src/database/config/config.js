6ef7404e1f530760c6913284a836c9067def5b3c
GuillePujals <guille_pujals@yahoo.com.ar>
2021-07-17 16:24:27 -0300

    Cambios para heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,10 +4,14 @@ module.exports =
 {
   "development": {
 
-    "username": process.env.DB_USERNAME,
+    /*"username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
+    "host": process.env.DB_HOST,*/
+    "username": "be746c8c643c0d",
+    "password": "de6c190e",
+    "database": "heroku_a537d65da4a5405",
+    "host": "us-cdbr-east-04.cleardb.com",
     "port": process.env.DB_PORT,
     "dialect": process.env.DB_DIALECT,
   },
