6ffb87b6a5e7dfe8309628310384e48989cbccc5
GuillePujals <guille_pujals@yahoo.com.ar>
2021-07-17 17:26:47 -0300

    correcciones

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,10 +4,15 @@ module.exports =
 {
   "development": {
 
-    /*"username": process.env.DB_USERNAME,
+    "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,*/
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT,
+  },
+  "production": {
+
     "username": "be746c8c643c0d",
     "password": "de6c190e",
     "database": "heroku_a537d65da4a5405",
