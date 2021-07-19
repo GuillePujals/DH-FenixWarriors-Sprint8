2c071cbb8d9d23e166129a802bb8004572880ab4
GuillePujals <guille_pujals@yahoo.com.ar>
2021-07-18 22:45:04 -0300

    Revisión back

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -13,10 +13,14 @@ module.exports =
   },
   "production": {
 
-    "username": "be746c8c643c0d",
-    "password": "de6c190e",
-    "database": "heroku_a537d65da4a5405",
-    "host": "us-cdbr-east-04.cleardb.com",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    //"username": "be746c8c643c0d",
+    //"password": "de6c190e",
+    //"database": "heroku_a537d65da4a5405",
+    //"host": "us-cdbr-east-04.cleardb.com",
     "port": process.env.DB_PORT,
     "dialect": process.env.DB_DIALECT,
   },
