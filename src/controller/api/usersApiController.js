4714cc7fecb3d68768135adeceba9fe789845155
santinocaffarone <santinocaffarone@mac.com>
2021-07-29 17:06:24 -0300

    caffarone- api-users

---
+++ src/controller/api/usersApiController.js
@@ -0,0 +1,39 @@
+const {Category, Destination, Image, Property, User} = require('../../database/models');
+
+
+const userApiControler ={
+    list: async (req, res) => {
+        
+        //Busco los usuarios
+        let users = await User.findAll()
+        let usuarios = []
+
+        //Creo el objeto usuario requerido
+       for (let i = 0; i < users.length; i++) {
+         //console.log(users[i]);
+         let usuario = {
+             id: users[i].id,
+             name: users[i].first_name + " " + users[i].last_name,
+             email: users[i].mail, 
+             detail: "http://localhost:3000/api/properties/"+users[i].id
+
+         }
+         usuarios.push(usuario)
+           
+       }
+
+        //Respuesta
+        let respuesta = {
+            meta: {
+                status: 200,
+                count: users.length
+            },
+            users: usuarios
+        }
+        res.json(respuesta)
+    }
+}
+
+
+
+module.exports = userApiControler;
