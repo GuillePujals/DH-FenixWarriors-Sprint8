1e20b2f84ba08774a6941b3f423b95f3a9b25cd0
Andres Ottonello <andres.ottonello@edessa.com.ar>
2021-08-02 18:16:50 -0300

    api/users/id v1

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use('/api/users',userApiRouter)
 app.use('/', userRouter);
 app.use('/', homeRouter);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
     console.log('Servidor corriendo en el puerto' + PORT)
 }
