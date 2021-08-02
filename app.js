41880dbc200dbdde2c5093d2454d718c9cec8dc9
GuillePujals <guille_pujals@yahoo.com.ar>
2021-08-02 10:35:42 -0300

    Correcciones

--- app.js
+++ app.js
@@ -53,6 +53,9 @@ app.use('/api/users',userApiRouter)
 app.use('/api/category',categoryApiRouter);
 app.use('/', userRouter);
 app.use('/', homeRouter);
+app.use((req, res, next) => {
+	res.status(404).send('<h1>404: Not Found</h1>')
+});
 
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
