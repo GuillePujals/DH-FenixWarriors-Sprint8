2a4ead5ed181963688e28c7799b350f0138a4598
santinocaffarone <santinocaffarone@mac.com>
2021-08-05 13:56:42 -0300

    caffarone propiedades por categoría

--- src/controller/api/categoriesApiController.js
+++ src/controller/api/categoriesApiController.js
@@ -4,49 +4,28 @@ const sequelize = db.sequelize;
 
 const categoriesApiController ={
     list: async (req, res) => {
-
+        
         //Traigo la info de la base de datos
-        try {let categories = await Category.findAll(
-            {
-                attributes: ["category", [sequelize.fn('COUNT', sequelize.col('category_id')), 'Count']],
-                include: ['properties']
-            }
-        );
+       Category.findAll({})
+            .then(categories =>{
 
-            //Genero la respuesta
-            let countByCategory = [
-                categories[0].properties.length,
-                categories[1].properties.length,
-                categories[2].properties.length,
-                categories[3].properties.length,
-                categories[4].properties.length
-            ]
-
-            let respuesta = {
+                let respuesta = {
                     meta: {
                         status: 200,
-                        count: categories.length,
-                        countPropr: properties.length
+                        count: categories.length
                     },
-                    //countByCategory,
-                    /*
-                    estrellas1: countByCategory[0],
-                    estrellas2: countByCategory[1],
-                    estrellas3: countByCategory[2],
-                    estrellas4: countByCategory[3],
-                    estrellas5: countByCategory[4],
-                    */
-                    categories: categories,
-                    
-                    //properties: properties
-                    
+                    categories: categories
                 }
-            console.log(respuesta);
-            res.json(respuesta);
-        } catch(error) {res.json({
-            status: "error 500",
-            message: error});
-        } 
+                res.json(respuesta);
+                })
+            .catch(error => {
+                res.json({
+                    meta:{
+                        status: 500,
+                        message: error
+                    }
+                })
+            });
     },
     count: (req, res) => {
         Category.findAll({
