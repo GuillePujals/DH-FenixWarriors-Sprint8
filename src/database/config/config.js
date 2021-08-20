d783511cd71cc6b644d566a0b2786c0b1565896d
GuillePujals <guille_pujals@yahoo.com.ar>
2021-08-20 19:27:20 -0300

    conexión heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,12 +4,12 @@ module.exports =
 {
   "development": {
 
-    "username": 'root',
-    "password": '',
-    "database": 'dbvacations',
-    "host": '127.0.0.1',
-    "port": 3306,
-    "dialect": 'mysql',
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT,
   },
   "production": {
 
